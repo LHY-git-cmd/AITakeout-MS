82b639e02aec4b719865601cfe473931e82d126d
lhy <2856599785@qq.com>
2026-06-25 00:08:26 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -85,7 +85,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         //创建一个消息转换器对象
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         //需要为消息转换器设置一个对象，对象转换器可以将java对象序列化为json数据
-        converter.setJsonPrefix(new JacksonObjectMapper().toString());
+        converter.setObjectMapper(new JacksonObjectMapper());
         //将自己的消息转化器加入容器中
         converters.add(0,converter);
     }
