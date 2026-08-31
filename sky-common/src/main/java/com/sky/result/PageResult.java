53c47733eb08b2b77e751285c70b312161428a72
lhy <2856599785@qq.com>
2026-08-31 23:00:24 +0800

    添加Swagger文档注释

--- sky-common/src/main/java/com/sky/result/PageResult.java
+++ sky-common/src/main/java/com/sky/result/PageResult.java
@@ -1,5 +1,6 @@
 package com.sky.result;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,16 +15,19 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "分页查询结果")
 public class PageResult implements Serializable {
 
     /**
      * 总记录数
      */
+    @Schema(description = "总记录数")
     private long total;
 
     /**
      * 当前页数据集合
      */
+    @Schema(description = "当前页数据集合")
     private List records;
 
 }
